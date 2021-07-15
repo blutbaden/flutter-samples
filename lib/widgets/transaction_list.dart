716cb41684b38fb883eec122225f84a276d23d3d
grimwolf <grimwolf@engineer.dev>
2021-07-15 16:34:38 +0100

    init

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 450,
+        child: transactions.isEmpty
+            ? Column(
+                children: [
+                  Text('No transaction added yet!',
+                      style: Theme.of(context).textTheme.title),
+                  Container(
+                      height: 150,
+                      child: Image.asset('assets/images/waiting.png',
+                          fit: BoxFit.cover)),
+                ],
+              )
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 5,
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: Text(
+                                '\$${transactions[index].amount.toStringAsFixed(2)}')),
+                      ),
+                      title: Text(
+                        transactions[index].title,
+                        style: Theme.of(context).textTheme.title,
+                      ),
+                      subtitle: Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Theme.of(context).errorColor,
+                        onPressed: () => deleteTx(transactions[index].id),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: transactions.length));
+  }
+}
